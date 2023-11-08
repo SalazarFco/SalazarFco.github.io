5d4b19b2d9c7b6d13d4e4e45064a998edc7ee8bb
Francisco Salazar <149215896+SalazarFco@users.noreply.github.com>
2023-11-08 20:25:46 -0300

    Delete sdsd.js

--- sdsd.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-function solve(meal_cost, tip_percent, tax_percent) {
-    x = Math.round(meal_cost + meal_cost*tip_percent/100 + meal_cost*tax_percent/100)
-   return x // Write your code here
-
-}
-
-console.log(solve(12.00,20,8))
-console.log(12.00 + 2)
